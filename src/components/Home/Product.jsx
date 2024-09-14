26c7496097ec526e9c12209b33fd01b7998d923c
Baha Tarlacı <bahatarlaci3@gmail.com>
2024-09-14 23:13:44 +0300

    Style update.

--- src/components/Home/Product.jsx
+++ src/components/Home/Product.jsx
@@ -4,7 +4,7 @@ const Product = ({ product }) => {
     return (
         <div className='border rounded-md relative cursor-pointer'>
             <img src={product.image} alt={product.name} className='w-full h-[250px] object-cover m-auto' />
-            <div className='text-2xl font-bold absolute top-0 right-0 z-10 bg-black text-white rounded-md p-1 m-1'>
+            <div className='text-2xl font-bold absolute top-0 right-0 z-10 bg-black text-white rounded-md p-1'>
                 {product?.price} <span className='text-sm'>TL</span>
             </div>
             <div className='text-center px-3 my-3 text-l font-bold'>
