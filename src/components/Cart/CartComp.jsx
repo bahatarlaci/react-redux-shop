e4daf55de9cb2a77df25890b00e2d842bf2ea57c
Baha Tarlacı <bahatarlaci3@gmail.com>
2024-09-15 20:13:42 +0300

    CartComp dev.

--- src/components/Cart/CartComp.jsx
+++ src/components/Cart/CartComp.jsx
@@ -3,17 +3,19 @@ import { removeFromCart } from '../../redux/cartSlice'
 import { useDispatch } from 'react-redux'
 import { formatPrice } from '../../utils/format'
 import { removeFromFavorite } from '../../redux/favoriteSlice'
+import { useNavigate } from 'react-router-dom'
 
 const CartComp = ({ cart, favorite }) => {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
   return (
     <>
       <div className='mt-10 grid grid-cols-1 lg:grid-cols-12 gap-6 items-center border-b pb-5'>
-        <div className='lg:col-span-2 flex justify-center lg:justify-start'>
+        <div onClick={() => navigate('/products/' + cart?.id)} className='lg:col-span-2 flex justify-center lg:justify-start cursor-pointer'>
           <img className='w-[150px] h-[150px] object-cover' src={cart?.image} alt={cart.title} />
         </div>
 
-        <div className='lg:col-span-5'>
+        <div className='lg:col-span-5 cursor-pointer' onClick={() => navigate('/products/' + cart?.id)}>
           <div className='text-xl font-semibold mb-2'>{cart?.title}</div>
           <div className='text-gray-600'>{cart?.description}</div>
         </div>
