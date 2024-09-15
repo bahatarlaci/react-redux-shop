1aa57dad4ad7452300193d9886f2664609a3dbb1
Baha Tarlacı <bahatarlaci3@gmail.com>
2024-09-15 19:37:49 +0300

    Responsive

--- src/components/Home/Category.jsx
+++ src/components/Home/Category.jsx
@@ -13,10 +13,16 @@ const Category = ({ setCategory }) => {
   return (
     <div className='bg-gray-100 p-4 mt-5'>
       <div className='text-xl font-bold mb-2 border-b px-2 pb-3'>Categories</div>
-      <div className='flex'>
+      <div className='flex flex-wrap md:flex-nowrap space-y-2 md:space-y-0 md:space-x-4'>
         {
           categories.map((category, index) => (
-            <div onClick={() => setCategory(category)} className='text-l px-2 py-3 cursor-pointer hover:bg-gray-200' key={index}>{category}</div>
+            <div
+              onClick={() => setCategory(category)}
+              className='text-l px-2 py-3 cursor-pointer hover:bg-gray-200 w-full md:w-auto text-center'
+              key={index}
+            >
+              {category}
+            </div>
           ))
         }
       </div>
@@ -24,4 +30,4 @@ const Category = ({ setCategory }) => {
   )
 }
 
-export default Category
+export default Category;
